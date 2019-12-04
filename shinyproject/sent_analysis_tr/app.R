5b815b4e5eb62c592486d21904d2fc8979931dcc
mrtkp9993 <mkoptur3@gmail.com>
2019-12-04 21:58:26 +0300

    Update app.R

--- shinyproject/sent_analysis_tr/app.R
+++ shinyproject/sent_analysis_tr/app.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(shinycssloaders)
 library(shinythemes)
 library(text2vec)
 library(dplyr)
@@ -11,7 +12,23 @@ load(file = "v_vectorizer.rda")
 
 ui <- fluidPage(
     theme = shinytheme("united"),
+    tags$head(
+        tags$link(rel = "stylesheet",
+                  href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css")
+    ),
     titlePanel("Türkçe Duygu Analizi"),
+    hr(),
+    tags$a(
+        href = "https://github.com/mrtkp9993",
+        target = "_blank",
+        class = "fa fa-2x fa-github"
+    ),
+    HTML("&nbsp;"),
+    tags$a(
+        href = "https://www.linkedin.com/in/muratkoptur/",
+        target = "_blank",
+        class = "fa fa-2x fa-linkedin"
+    ),
     hr(),
     sidebarLayout(
         sidebarPanel(
